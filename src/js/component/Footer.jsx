ed161a86eb784831581518e41179a1298896926b
Ricard Mesegué <ricardm1995@gmail.com>
2024-08-19 19:23:48 +0000

    feat: Footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 export const Footer = () => {
     return (
-        <p>Soy un footer</p>
+        <div className="bg-dark text-white text-center py-3 mt-auto mb-auto">
+            <div className="container p-5">
+                <div className="row">
+                    <p className="mx-auto">Copyright @ Your Website 2024</p>
+                </div>
+            </div>
+        </div>
     )
 }
