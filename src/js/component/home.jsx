6a665967af5c236566407a6ad34f5b20e6330829
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-08-19 19:05:33 +0000

    feat: home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,25 +4,18 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./Navbar.jsx";
 import { Card } from "./Card.jsx";
+import { Footer } from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
+			<div className="d-flex flex-column min-vh-100">
 			<Navbar/>
 			<Card/>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			</div>
+			
 		</div>
 	);
 };
